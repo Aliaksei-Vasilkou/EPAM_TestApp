85df6591d5b0fe5c9b57d4beadfe698603f035f9
Vasilkov Alexey <coyc@inbox.ru>
2017-09-02 15:11:09 +0300

    Working application

--- app/src/main/java/com/example/coyc/epam_testapplication/MainActivity.java
+++ app/src/main/java/com/example/coyc/epam_testapplication/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.coyc.epam_testapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     Button button;
+    Toast toast;
 
 
     @Override
@@ -24,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 textView.setText(R.string.tv_msg);
+
+                toast = Toast.makeText(getApplicationContext(), R.string.toast_msg, Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, -50);
+                toast.show();
             }
         });
     }
